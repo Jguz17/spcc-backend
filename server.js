bcc3ae2c993234d96ae9d44b3e1657b26f3a70cb
jguz17 <johnathanguzman17@gmail.com>
2020-11-30 23:09:25 -0600

    working on routes and models

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ connectDB()
 
 app.use(cors({origin:true,credentials: true}));
 
+app.use(express.json({ extended: false }))
+
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/booking', require('./routes/booking'))
+app.use('/api/messages', require('./routes/messages'))
+app.use('/api/users', require('./routes/users'))
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
